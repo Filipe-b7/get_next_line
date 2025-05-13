9200a4ff11b2d4c8ef58e5faa8dd5f133e09c99f
frocha-b <frocha-b@student.42porto.comm>
2025-05-13 11:51:12 +0100

    a

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: frocha-b <frocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 17:32:25 by frocha-b          #+#    #+#             */
-/*   Updated: 2025/05/08 20:14:09 by frocha-b         ###   ########.fr       */
+/*   Updated: 2025/05/13 11:28:32 by frocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,31 @@
 char	*get_next_line(int fd)
 {
 	static char	buff[BUFFER_SIZE + 1];
+	char		buff_segment[BUFFER_SIZE + 1];
 	char		*line;
 	int			bytes_read;
 	char		*temp;
-	char		*buff_resized;
 
-	if (!fd || BUFFER_SIZE <= 0 )
-		return (NULL);
-	line[0] = '\0';
-	
+	if (fd < 0 || BUFFER_SIZE <= 0 )
+		return (NULL);	
+
+	line = NULL;	
 	while(1)
 	{
 		bytes_read = read(fd, buff, BUFFER_SIZE);
-		buff[bytes_read] = '\0'
 		if (bytes_read < 0)
 		{
 			free (line);
 			return (NULL);
 		}
+		buff[bytes_read] = '\0';
 		if (bytes_read == 0)
 			return line;
-		if (str_srch(buff, "\0", bytes_read) || str_srch(buff, "\n", bytes_read))
+		if (str_srch(buff, '\0', bytes_read) == 1 || str_srch(buff, '\n', bytes_read) == 1)
 		{
-			buff_resized = copy_buff(buff, bytes_read);
-			ft_strjoin(line, buff_resized);
-			shift_buffer(buff, bytes_read);
+			copy_buff(buff_segment, buff, bytes_read);
+			line = ft_strjoin(line, buff_segment);
+			shift_buffer(buff, bytes_read);	
 			return (line);
 		}
 			else
@@ -54,11 +54,15 @@ char	*get_next_line(int fd)
 }
 
 #include <stdio.h>
+#include <fcntl.h>
 
 int main(void)
 {
+	int	fd = open("teste.txt",O_RDONLY);
 
-	printf("%s",get_next_line(3));
+	if (fd == -1)
+		return 1;
+	printf("%s",get_next_line(fd));
+	close(fd);
 	return (0);
-
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: frocha-b <frocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 17:32:22 by frocha-b          #+#    #+#             */
-/*   Updated: 2025/05/07 17:32:22 by frocha-b         ###   ########.fr       */
+/*   Updated: 2025/05/13 11:30:14 by frocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,37 +17,43 @@ int	ft_strlen(char *str)
 	int len;
 
 	len = 0;
+	if (!str)
+		return 0;
 	while(str[len])
 		len++;
-	return (len)
+	return (len);
 }
-char	*ft_strjoin(char *line, char *buff)
-{
-	char	*str;
-	int		length;
-	int		i;
-	int		j;
-
-	length = ft_strlen(line) + ft_strlen(buff);
-	str = malloc(length + 1* sizeof(char));
-	if (!str)
-		free(line);
-		return (NULL);
-	while (line[i])
+	char	*ft_strjoin(char *line, char *buff)
 	{
-		str[i] = line[i];
-		i++;
-	}
-	while (buff[j])
-	{
-		str[i + j] = buff[j];
-		j++;
-	}
-	str[i + j] = '\0';
-	return (str);
-}
+		char	*str;
+		int		length;
+		int		len_line;
+		int		i;
+		int		j;
 
-int	str_srch(char * buff, char c, int bytes_read)
+		len_line = 0;
+		length = ft_strlen(line) + ft_strlen(buff);
+		str = malloc(length + 1);
+		if (!str)
+			free(line);
+			return (NULL);
+		i = 0;
+		while (i < ft_strlen(line))
+		{
+			str[i] = line[i];
+			i++;
+		}
+		j = 0;
+		while (j < ft_strlen(buff))
+		{
+			str[i + j] = buff[j];
+			j++;
+		}
+		str[i + j] = '\0';
+		return (str);
+	}
+
+int	str_srch(char *buff, char c, int bytes_read)
 {
 	int	i;
 
@@ -56,29 +62,43 @@ int	str_srch(char * buff, char c, int bytes_read)
 	{
 		if (buff[i] == c)
 			return (1);
+		i++;
 	} 
 	return (0);
 }
 
 void	shift_buffer(char *buff, int bytes_read)
 {
-	while (buff[i] != '\n' && i < bytes_read)
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (buff[i] != '\n' &&  buff[i] != '\0' && i < bytes_read)
 		i++;
-	while (buff[i + j])
+	i++;
+	while (i + j < bytes_read)
 	{
 		buff[j] = buff[i + j];
 		j++;
 	}
-	buff[i + j] = '\0';
+	buff[j] = '\0';
 }
 
-void	copy_buff(char *buff, int bytes_read)
+void	copy_buff(char *buff_segment,char *buff, int bytes_read)
 {
-	char	*buff_resized;
-	while (buff[i] != '\n' && i < bytes_read)
+	int	i;
+
+	i = 0;
+	while (buff[i] && buff[i] != '\n' && i < bytes_read)
 	{
-		buff_resized[i] = buff[i];
+		buff_segment[i] = buff[i];
 		i++;
 	}
-
+	if (buff[i] == '\n' && i < bytes_read)
+	{
+		buff_segment[i] = '\n';
+		i++;
+	}
+	buff_segment[i] = '\0';
 }
